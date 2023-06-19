03b3256e031cc438c50d531e0f3228da2dde5a6e
HectorGinory <hectorginorydevs@gmail.com>
2023-06-19 15:05:40 +0200

    feat: Added an input to bring pokemon by criteria and his handler

--- src/app/api/pokeAPI.js
+++ src/app/api/pokeAPI.js
@@ -2,15 +2,20 @@ import axios from "axios";
 
 export const bring20PokemonByNumberPage = async (page) => {
     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${page*20}&limit=20`)
-    const pokemonList = []
-    for (let i = 0; i < res.data.results.length; i++) {
-        const pokemonData = await axios.get(res.data.results[i].url)
-        pokemonList.push(pokemonData.data)
-    }
+    const pokemonList = await bringDetailInfoPokemonList(res.data.results)
     const data = {
         totalPokemon: res.data.count,
         pageNumber: page,
         pokemonList: pokemonList
     }
     return data
+}
+
+const bringDetailInfoPokemonList = async (results) => {
+    const pokemonList = []
+    for (let i = 0; i < results.length; i++) {
+        const pokemonData = await axios.get(results[i].url)
+        pokemonList.push(pokemonData.data)
+    }
+    return pokemonList
 }
--- src/app/pokedex/page.js
+++ src/app/pokedex/page.js
@@ -1,27 +1,38 @@
-'use client'
+"use client";
 import PokemonCard from "@/components/pokemonCard/PokemonCard";
 import { useEffect, useState } from "react";
 import { bring20PokemonByNumberPage } from "../api/pokeAPI";
 
 export default function Pokedex() {
-    const [pokemonList ,setPokemonList] = useState([])
-    const [page, setPage] = useState(0)
+  const [pokemonList, setPokemonList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [criteria, setCriteria] = useState("");
 
-    useEffect(()=> {
-      bring20PokemonByNumberPage(page).then(res => {
-        setPokemonList(res.pokemonList)
-      })
-    },[])
+  useEffect(() => {
+    bring20PokemonByNumberPage(page).then((res) => {
+      setPokemonList(res.pokemonList);
+    });
+  }, []);
 
-    return (
-      <main>
-      {pokemonList.length > 0 &&
-      pokemonList.map((pokemon, index) => {
-        return (
-          <PokemonCard pokemon={pokemon} key={index}/>
-        )
-      })
-      }
-      </main>
-    );
-  }
+
+  const criteriaHandler = (e) => {
+    setCriteria(e.target.value);
+  };
+
+  return (
+    <main>
+      <input
+        name="criteria"
+        placeholder="Ej.: Bulbasaur"
+        type="text"
+        onChange={(e) => criteriaHandler(e)}
+      />
+      <div className="pokemonCardContainer">
+        {pokemonList.length > 0 &&
+          pokemonList.map((pokemon, index) => {
+            return <PokemonCard pokemon={pokemon} key={index} />;
+          })}
+      </div>
+    </main>
+  );
+}
