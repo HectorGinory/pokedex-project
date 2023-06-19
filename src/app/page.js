8af97add7705d0246db8c0c63fa0232b611dbc86
HectorGinory <hectorginorydevs@gmail.com>
2023-06-19 13:29:33 +0200

    feat: basic structure of the project created

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main>
+      <div className="main-menu">
+        <h1>Pokemon Web</h1>
+      </div>
     </main>
-  )
+  );
 }
---
+++ src/app/pokedex/page.js
@@ -0,0 +1,11 @@
+'use client'
+import { useEffect, useState } from "react";
+
+export default function Pokedex() {
+    const [pokemonList ,setPokemonList] = useState([])
+
+    return (
+      <main>
+      </main>
+    );
+  }
