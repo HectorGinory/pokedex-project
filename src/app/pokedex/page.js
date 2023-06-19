282c6ab21ca19f2adbd81a12c6c9e3a508945a04
HectorGinory <hectorginorydevs@gmail.com>
2023-06-19 13:46:18 +0200

    feat: added useEffect to setPokemonList hook with all the pokemonData

--- src/app/pokedex/page.js
+++ src/app/pokedex/page.js
@@ -3,7 +3,14 @@ import { useEffect, useState } from "react";
 
 export default function Pokedex() {
     const [pokemonList ,setPokemonList] = useState([])
+    const [page, setPage] = useState(0)
 
+    useEffect(()=> {
+      bring20PokemonByNumberPage(page).then(res => {
+        setPokemonList(res.pokemonList)
+      })
+    },[])
+    
     return (
       <main>
       </main>
